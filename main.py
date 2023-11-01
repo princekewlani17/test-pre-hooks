7000b1b4ce8235eb74b090f34a71749d5213894a
Kewlani, Prince <princekewlani17@gmail.com>
2023-11-01 20:50:48 +0530

    test pre commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 '''
 #! usr/bin/python3
 
-print("Hello, Test!")
+print("Hello, Testo!")
 
 print("This is multi character line test > 84 chars: 0,1,2,3, \
       4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24, \
